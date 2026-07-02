5e2f7425a627d35bb5254652baa27be1b88076cd
Raheenarwork <raheenarwork>
2026-07-02 13:07:15 +0530

    array

--- me.cpp
+++ me.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
-    cout << "raheen" << endl;
+    int size = 5 ;
+    int marks[size];
+    for (int i= 0 ; i<size;i++)
+    {
+        cin>>marks[i];
+    }
+    for (int i=0;i<size;i++)
+    {
+        cout<<marks[i]<<endl;
+    }
     return 0;
 }
